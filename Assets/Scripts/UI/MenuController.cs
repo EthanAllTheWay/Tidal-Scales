17c084fbc6e9a769024b4befcc22b85001e77c5b
Ethan <73133148+EthanAllTheWay@users.noreply.github.com>
2023-11-07 16:21:32 -0400

    Menu Mostly Functional
    All the menus are working besides the levels loading the game scene.

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject startMenu;
+    
+    [SerializeField]
+    private GameObject mainMenu;
+
+    [SerializeField]
+    private GameObject settingsMenu;
+
+    [SerializeField]
+    private GameObject quitConfirmationMenu;
+
+    [SerializeField]
+    private GameObject levelSelectionMenu;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        init();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitFunction()
     {
-        
+        Debug.Log("Quit Function Called.");
+        Application.Quit();
+    }
+
+    public void StartFunction()
+    {
+        LoadMenu(startMenu, mainMenu);
+    }
+
+    public void SettingsFunction()
+    {
+        LoadMenu(mainMenu, settingsMenu);
+    }
+
+    public void LevelSelectionFunction()
+    {
+        LoadMenu(mainMenu, levelSelectionMenu);
+    }
+
+    public void QuitConfirmationFunction()
+    {
+        LoadMenu(mainMenu, quitConfirmationMenu);
+    }
+
+    public void BackFunction()
+    {
+        if (levelSelectionMenu.activeInHierarchy)
+        {
+            LoadMenu(levelSelectionMenu, mainMenu);
+        }
+        else if (settingsMenu.activeInHierarchy)
+        {
+            LoadMenu(settingsMenu, mainMenu);
+        }
+        else if (quitConfirmationMenu.activeInHierarchy)
+        {
+            LoadMenu(quitConfirmationMenu, mainMenu);
+        }
+    }
+
+    public void LoadMenu(GameObject menuToDeactivate, GameObject menuToActivate)
+    { 
+        menuToDeactivate.SetActive(false);
+        menuToActivate.SetActive(true);
+    }
+
+    private void init()
+    {
+        startMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+        quitConfirmationMenu.SetActive(false);
+        mainMenu.SetActive(false);
+        levelSelectionMenu.SetActive(false);
     }
 }
