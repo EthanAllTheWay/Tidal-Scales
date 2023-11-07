9c5776161178a4bae3fe35f6b4a5d92f2644944e
RyuzakiHG <145231014+RyuzakiHG@users.noreply.github.com>
2023-11-07 17:00:50 -0600

    Score System
    Simple score system
    - Added "ScoreSystem" script, keeps score to 0 at start, and updates UI score
    -Modified "Indicator" script, when a fish touches it, we add one point
    
    -Added a "Fish" tag, for the fish prefab

--- Assets/Scripts/Indicator.cs
+++ Assets/Scripts/Indicator.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Indicator : MonoBehaviour
 {
+    private float score;
     /*
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(Conductor.instance.songPositionInBeats);
     }*/
+    
+    
+    protected void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Fish") {
 
+            score = PlayerPrefs.GetFloat("Score");
+            score += 1;
+            PlayerPrefs.SetFloat("Score", score);
+            Debug.Log("Score: " + score);
+            
+        }
+    }
+   
 }
