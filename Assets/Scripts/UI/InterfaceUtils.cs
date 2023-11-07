484a6668a76213f5d932c4f7035573bf40a28a28
Ethan <73133148+EthanAllTheWay@users.noreply.github.com>
2023-11-07 16:50:09 -0400

    Menu is Full Functional
    The level 01 button now switches the scene over to the game scene. I added in some titles for every menu, and the other levels are now not interactable since they are not complete.

---
+++ Assets/Scripts/UI/InterfaceUtils.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InterfaceUtils : MonoBehaviour
+{
+    public static void LoadScene(int sceneIndex)
+    { 
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
