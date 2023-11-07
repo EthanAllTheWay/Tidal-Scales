5c78759dc40dbebfe7243696ea24d7f738032c9f
d3val <devalt5008@gmail.com>
2023-11-07 17:13:03 -0600

    Inputs added
    When a fish is inside the indicator and the player performs the trigger, fish is destroyed

--- Assets/Scripts/Indicator.cs
+++ Assets/Scripts/Indicator.cs
@@ -1,13 +1,81 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+[Serializable]
+public enum trigger
+{
+    leftTrigger,
+    middleLeftTrigger,
+    middleRightTrigger,
+    rightTrigger
+}
 
 public class Indicator : MonoBehaviour
 {
-    /*
+    // Inputs mapping
+    public InputActionAsset primaryInputs;
+    InputActionMap gameplayActionMap;
+    InputAction triggerAction;
+    //This specify what action is gonna be used by the indicator
+    public trigger actionIndex;
+    private Fish currentFish = null;
+
+    //A dictionary that I use to find the name of the action specified by the index
+    Dictionary<int, string> inputActionDictionary = new Dictionary<int, string>()
+    {
+        {0, "Left trigger"},
+        {1, "Left middle trigger"},
+        {2 ,"Right middle trigger" },
+        {3, "Right trigger"}
+    };
+
+    private void Awake()
+    {
+        // We read information from the inputs mapping
+        gameplayActionMap = primaryInputs.FindActionMap("Gameplay");
+        triggerAction = gameplayActionMap.FindAction(inputActionDictionary[(int)actionIndex]);
+
+        // We specify what method will be invoked at what time (performed in this case)
+        triggerAction.performed += ctx => Capture();
+    }
+
+    // Input system needs enable and disable the action in order to work.
+    private void OnEnable()
+    {
+        triggerAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        triggerAction.Disable();
+    }
+
+    // Destroys the fish that is inside the indicator
+    private void Capture()
+    {
+        if (currentFish != null)
+        {
+            Destroy(currentFish.gameObject);
+            currentFish = null;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(Conductor.instance.songPositionInBeats);
-    }*/
+        if (other.gameObject.CompareTag("Fish"))
+        {
+            currentFish = other.GetComponent<Fish>();
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Fish"))
+        {
+            currentFish = null;
+        }
+    }
 }
