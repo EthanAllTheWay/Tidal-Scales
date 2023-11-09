5c37ca908378a9982d312536b69ee2f7f91b098c
d3val <devalt5008@gmail.com>
2023-11-08 21:30:53 -0600

    First attempt to map beats

--- Assets/Scripts/Mapper.cs
+++ Assets/Scripts/Mapper.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Mapper : MonoBehaviour
 {
+    string fileLocation;
+    string data = "";
+
     int i = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fileLocation = Application.dataPath + "/noteBeats";
     }
 
     // Update is called once per frame
@@ -16,8 +20,16 @@ public class Mapper : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log(i+" : "+  Conductor.instance.songPositionInBeats);
+            Debug.Log(i + " : " + Conductor.instance.songPositionInBeats);
+            data += Conductor.instance.songPositionInBeats.ToString() + "\n";
+            Write(data);
             i++;
-        }   
+        }
     }
+
+    public void Write(string s)
+    {
+        File.WriteAllText(fileLocation, s);
+    }
+
 }
