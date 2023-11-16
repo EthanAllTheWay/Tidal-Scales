67f1d987fd84c0dd3da4c8620d4392c04453fb59
Ethan <73133148+EthanAllTheWay@users.noreply.github.com>
2023-11-16 12:55:55 -0400

    Fixed the merge issues on this branch.

--- Assets/Scripts/Indicator.cs
+++ Assets/Scripts/Indicator.cs
@@ -76,16 +76,22 @@ public class Indicator : MonoBehaviour
         if (currentFish != null)
         {
             // Hit
-            SoundEffects.instance.catchSound.Play();
+
+            if (SoundEffects.instance != null && SoundEffects.instance.catchSound != null)
+            {
+                SoundEffects.instance.catchSound.Play();
+            }
             score.addScore(currentFish.beatOfThisNote); // Calls score system to work
             Destroy(currentFish.gameObject);
             currentFish = null;
         }
         else
         {
-
             // Miss
-            SoundEffects.instance.missSound.Play();
+            if (SoundEffects.instance != null && SoundEffects.instance.missSound != null)
+            {
+                SoundEffects.instance.missSound.Play();
+            }
             score.multiplier = 1; //If you press a button when there isn't any fish, multiplier resets
         }
     }
--- Assets/Scripts/SoundEffects.cs
+++ Assets/Scripts/SoundEffects.cs
@@ -19,29 +19,4 @@ public class SoundEffects : MonoBehaviour
         }
         instance = this;
     }
-
-    /**
-    * Creates a sub clip from an audio clip based off of the start time
-    * and the stop time. The new clip will have the same frequency as
-    * the original.
-    */
-    private AudioClip MakeSubclip(AudioClip clip, float start, float stop)
-    {
-        /* Create a new audio clip */
-        int frequency = clip.frequency;
-        float timeLength = stop - start;
-        int samplesLength = (int)(frequency * timeLength);
-        AudioClip newClip = AudioClip.Create(clip.name + "-sub", samplesLength, 1, frequency, false);
-
-        /* Create a temporary buffer for the samples */
-        float[] data = new float[samplesLength];
-        /* Get the data from the original clip */
-        clip.GetData(data, (int)(frequency * start));
-        /* Transfer the data to the new clip */
-        newClip.SetData(data, 0);
-
-        /* Return the sub clip */
-        return newClip;
-    }
-
 }
