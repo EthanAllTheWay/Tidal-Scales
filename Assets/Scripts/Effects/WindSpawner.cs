2181d371f6978cc63c9c088635adaf91141e2662
Ethan <73133148+EthanAllTheWay@users.noreply.github.com>
2023-11-20 15:35:20 -0400

    WindSpawner Now Using Both Wind Assets

--- Assets/Scripts/Effects/WindSpawner.cs
+++ Assets/Scripts/Effects/WindSpawner.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 
 public class WindSpawner : MonoBehaviour
 {
-    public GameObject windObject;
+    public GameObject[] windObjectList;
     public Quaternion windDirection;
     public Vector3 windSpawnRange;
     public int windDensity;
     private float windSpawnSpeed = 10; // Make sure this matches or is greater to the Wind particle duration time,
                                        // otherwise it will slowly eat up more and more memory. Reason being, it
                                        // will spawn the wind objects faster then they will get destroyed.
-
     private float elapsedTime = 0;
 
     // Start is called before the first frame update
@@ -28,17 +27,13 @@ public class WindSpawner : MonoBehaviour
         if (elapsedTime >= windSpawnSpeed)
         { 
             elapsedTime = 0;
-            Debug.Log("Wind Spawner Trigger.");
             for (int i = 0; i < windDensity; i++)
             {
-                Debug.Log("Density Check " + i);
-                //Instantiate(windObject);
-
                 Vector3 RandVector3 = new Vector3(
                     Random.Range(-windSpawnRange.x, windSpawnRange.x),
                     Random.Range(10, windSpawnRange.y),
                     Random.Range(0, -windSpawnRange.z));
-                GameObject windEffectObject = Instantiate(windObject, RandVector3, windDirection);
+                Instantiate(windObjectList[Random.Range(0, windObjectList.Length)], RandVector3, windDirection);
             }
         }
     }
