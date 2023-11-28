7131cd7c02233d1f345a6b4d430a0ac3a0bf04e6
d3val <devalt5008@gmail.com>
2023-11-28 12:13:19 -0600

    Floating score wrong spawn big fixed

--- Assets/Scripts/FloatingScore.cs
+++ Assets/Scripts/FloatingScore.cs
@@ -5,17 +5,9 @@ using UnityEngine;
 public class FloatingScore : MonoBehaviour
 {
     public float screenTime = 1f;
-    public Vector3 placement = new Vector3(0, 2, 0); 
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, screenTime);
-        transform.localPosition += placement;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
